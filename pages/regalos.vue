d20cb823228592828aa50350c0b752607e4c936d
Danielagzba <daniela.gbarron@gmail.com>
2020-01-13 12:56:37 -1000

    regalos

--- pages/regalos.vue
+++ pages/regalos.vue
@@ -1,23 +1,85 @@
 <template>
   <v-container>
-  <v-row>
-    <v-col col="12">
-      <v-row justify="center" align="center">
+    <v-row>
+
+      <v-col cols="12" xs="12" md="6">
+        <v-row align="start" justify="start">
+          <v-card
+            tile
+            class="pa-3"
+            width="700%"
+            color="transparent"
+            flat>
 
 
-        <v-card color="transparent"
-        flat
-        tile
-        class="pa-10">
+            <v-img
 
-          <p class="display-2 text-center font-weight-black primary--text">
-            PRÓXIMAMENTE
-          </p>
+              src="https://i.imgur.com/fLIek27.jpg"
+              aspect-ratio="1"
+              max-width="600"
+              max-height="500"
+            ></v-img>
 
-        </v-card>
-      </v-row>
-    </v-col>
-  </v-row>
+          </v-card>
+        </v-row>
+      </v-col>
+
+      <v-col cols="12" xs="12" md="6">
+        <v-row
+          align="center"
+          justify="center"
+
+        >
+          <!-- color="grey lighten-5" -->
+          <v-card
+
+            color="transparent"
+            class="ma-9 pa-9"
+            flat
+            tile
+
+
+          >
+
+            <p
+
+              class="display-2 text-center font-weight-black primary--text"
+
+            >
+
+              MESA DE REGALOS
+            </p>
+
+            <p class="text-justify">
+              Muchas gracias por formar parte de este día tan especial en nutras vidas. Tu presencia es lo más
+              importante para nosotros. Sin embargo si quieres contribuir en nuestro proyecto de vida agradecemos tu
+              regalo en:
+              <p class="text-center"> <a href="https://andreaybeto.zankyou.com/us" target="_blank">andreaybeto.zankyou.com</a>
+
+            </p>
+
+            <p class="text-center">
+              o
+              <p class="text-center font-weight-medium "> BBVA Andrea González Barrón
+
+            </P>
+            <p class="text-center">
+              Cuenta: 1560747338
+
+              <p class="text-center">Clabe: 012180015607473386
+            </p>
+
+
+          </v-card>
+        </v-row>
+      </v-col>
+
+      <v-col sm="12" xs="12" md="6">
+
+        <v-img> </v-img>
+      </v-col>
+
+    </v-row>
   </v-container>
 </template>
 
